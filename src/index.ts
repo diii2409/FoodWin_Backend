c7dc5d26a03020c3d132e6297d9672561f2bc320
diii2409 <duynhut366@gmail.com>
2024-09-22 02:49:46 +0700

    create user API

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import cors from "cors";
 import "dotenv/config";
-import express, {Request, Response} from "express";
+import express from "express";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-	res.json({message: "hello!"});
-});
+// app.get("/test", async ({req, res}: {req: Request; res: Response}) => {
+// 	res.json({message: "hello!"});
+// });
+
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
 	console.log("server started on localhot:7000");
