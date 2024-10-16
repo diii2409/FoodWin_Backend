b6dbce83b24f51760ec56cfe48ac77a2cab9d15a
diii2409 <duynhut366@gmail.com>
2024-10-16 23:55:13 +0700

    create my restaurant

--- src/controller/MyRestaurantController.ts
+++ src/controller/MyRestaurantController.ts
@@ -1,5 +1,32 @@
+import cloudinary from "cloudinary";
 import {Request, Response} from "express";
-const createRestaurant = async (req: Request, res: Response) => {};
+import mongoose from "mongoose";
+import Restaurant from "../models/restaurant";
+// create Restaurant
+const createRestaurant = async (req: Request, res: Response) => {
+	try {
+		const existingRestaurant = await Restaurant.find({user: req.userId});
+		if (existingRestaurant) {
+			return res.status(409).json({message: "User restaurant already exists"});
+		}
+		const image = req.file as Express.Multer.File;
+		const base64Image = Buffer.from(image.buffer).toString("base64");
+		const dataURL = `data:${image.mimetype};base64,${base64Image}`;
+
+		const uploadResponse = await cloudinary.v2.uploader.upload(dataURL);
+
+		const restaurant = new Restaurant(req.body);
+		restaurant.imageUrl = uploadResponse.url;
+		restaurant.user = new mongoose.Types.ObjectId(req.userId);
+		await restaurant.save();
+
+		res.status(201).send(restaurant);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({error: "Internal server error"});
+	}
+};
+
 const updateRestaurant = async (req: Request, res: Response) => {};
 const getRestaurant = async (req: Request, res: Response) => {};
 
--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -1,18 +1,24 @@
 import express from "express";
-import MyRestaurantController from "../controller/MyRestaurantController";
 import multer from "multer";
+import MyRestaurantController from "../controller/MyRestaurantController";
+import {jwtCheck, jwtParse} from "../middleware/auth";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({
-  storage,
-  limits: {
-    fileSize: 5 * 1024 * 1025 // 5MB ,
-    
-  }
-})
+	storage,
+	limits: {
+		fileSize: 5 * 1024 * 1025, // 5MB ,
+	},
+});
 
 // api/my/user
-router.post("/", MyRestaurantController.createRestaurant);
-router.put("/", MyRestaurantController.updateRestaurant);
-router.get("/", MyRestaurantController.getRestaurant);
+router.post(
+	"/",
+	jwtCheck,
+	jwtParse,
+	upload.single("imageFile"),
+	MyRestaurantController.createRestaurant,
+);
+router.put("/", jwtCheck, MyRestaurantController.updateRestaurant);
+router.get("/", jwtCheck, MyRestaurantController.getRestaurant);
